4779fcf26f26b49305b45fd6cea27564894a1da8
Edward Rees <erees.hk@gmail.com>
2026-09-02 15:48:50 -0700

    [feat] added madlibs starter code

---
+++ inclass/madlibs/madlibs.py
@@ -0,0 +1,9 @@
+noun = input("Give me a noun: ")
+verb = input("Give me a verb: ")
+adjective = input("Give me an adjective: ")
+color = input("Give me a color: ")
+place = input("Give me a place: ")
+
+print("I like", noun)
+print("He", verb, adjective)
+# ADD MORE
